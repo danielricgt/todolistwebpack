735cacafa87699e3dc246861eb721a8c146d735d
Daniel galvan <danielricardogt@gmail.com>
2025-03-24 13:42:17 -0500

    :construction: adding add task

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,18 +1,19 @@
-import '../css/componentes.css';
-// import webpacklogo from '../assets/img/webpack-logo.png';
 
+const divTodolist = document.querySelector(".todo-list")
+export const crearTodoHTML= (todo) => {
 
-export const saludar = ( nombre = 'sin nombre' ) => {
-    console.log('Creando etiqueta h1');
-
-    const h1 = document.createElement('h1');
-    h1.innerText = `Hola ${ nombre }`;
-
-    document.body.append( h1 );
-
-    
-    // Img
-    // const img = document.createElement('img');
-    // img.src = webpacklogo;
-    // document.body.append( img );
+    const htmlTodo = `
+    <li class=${todo.completado ? 'completed': ''} data-id=${todo.id}">
+	<div class="view">
+		<input class="toggle" type="checkbox" ${todo.completado ? 'checked' : ''}>
+		<label>${todo.tarea}</label>
+		<button class="destroy"></button>
+	</div>
+	<input class="edit" value="Create a TodoMVC template">
+</li>
+`
+const div =  document.createElement("div");
+div.innerHTML =  htmlTodo;
+divTodolist.append(div.firstElementChild);
+return div.firstElementChild;
 }
