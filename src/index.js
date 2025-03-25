ea94ca88ce6db37d9ee86ffc3fc648d959e06128
Daniel galvan <danielricardogt@gmail.com>
2025-03-24 19:34:20 -0500

    ":construction: adding todo since event listener"

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { TodoList, Todo } from './classes'
 import { crearTodoHTML } from './js/componentes';
 import './styles.css';
 
-const todoList = new TodoList();
+export const todoList = new TodoList();
 const tarea = new Todo('Comprar leche');
 todoList.nuevoTodo(tarea);
 tarea.completado = true;
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,5 +1,7 @@
-
-const divTodolist = document.querySelector(".todo-list")
+import { Todo } from "../classes";
+import {todoList} from "../index"
+const divTodolist = document.querySelector(".todo-list");
+const txtInput =   document.querySelector(".new-todo");
 export const crearTodoHTML= (todo) => {
 
     const htmlTodo = `
@@ -17,3 +19,18 @@ div.innerHTML =  htmlTodo;
 divTodolist.append(div.firstElementChild);
 return div.firstElementChild;
 }
+
+// Eventos
+
+txtInput.addEventListener("keyup", (event)=>
+{
+    if (event.keyCode === 13 && txtInput.value.length > 0) {
+        console.log(txtInput.value);
+        const nuevoTodo = new Todo(txtInput.value)
+        todoList.nuevoTodo(nuevoTodo);
+        console.log(todoList);
+        crearTodoHTML(nuevoTodo);
+        txtInput.value=('');  
+    }
+}
+)
